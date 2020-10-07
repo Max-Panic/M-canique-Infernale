e4509e57a491eb63c24d6bd7bcad5ca078e91971
Terry <terryperezgervais@gmail.com>
2020-10-07 13:08:08 +0200

    [Add] Unity project

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MidiJack;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    void Update()
+    {
+        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKey(kcode))
+                Debug.Log("KeyCode down: " + kcode);
+        }
+
+        for (int i = 0; i < 101; i++)
+        {
+            if (MidiMaster.GetKeyDown(i))
+            {
+                Debug.Log(i);
+            }
+        } 
+    }
+}
