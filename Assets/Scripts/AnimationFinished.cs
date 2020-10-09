f87c500f2ed4b1c984d794d34fa9f9343ad7292d
Rémi Moreaux <moreaux_rmi@yahoo.fr>
2020-10-09 13:54:11 +0200

    Boards + finished animator

---
+++ Assets/Scripts/AnimationFinished.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationFinished : MonoBehaviour
+{
+    private Animator truc;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        truc = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void KeyFinish()
+    {
+        truc.SetBool("KeyFinished",true);
+    }
+
+    public void WaterFinish()
+    {
+        truc.SetBool("WaterFinished", true);
+    }
+}
